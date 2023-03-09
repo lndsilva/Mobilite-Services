ab01d198ea7b5c79adb622fac00d4e58be75c85c
lndsilva <lndsilva@hotmail.com>
2023-03-09 13:08:15 -0300

    criando sistema folha de pagamento

--- Mobilitec-Services/Program.cs
+++ Mobilitec-Services/Program.cs
@@ -16,7 +16,7 @@ namespace Mobilitec_Services
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmCalculadoraSimples());
+            Application.Run(new frmFolhaPagamento());
         }
     }
 }
--- Mobilitec-Services/frmCalculadoraSimples.Designer.cs
+++ Mobilitec-Services/frmCalculadoraSimples.Designer.cs
@@ -35,15 +35,17 @@ namespace Mobilitec_Services
             this.txtNum1 = new System.Windows.Forms.TextBox();
             this.txtNum2 = new System.Windows.Forms.TextBox();
             this.gpbOperador = new System.Windows.Forms.GroupBox();
-            this.rdbAdicao = new System.Windows.Forms.RadioButton();
-            this.rdbsubtracao = new System.Windows.Forms.RadioButton();
-            this.rdbMultiplicacao = new System.Windows.Forms.RadioButton();
             this.rdbDivisao = new System.Windows.Forms.RadioButton();
+            this.rdbMultiplicacao = new System.Windows.Forms.RadioButton();
+            this.rdbsubtracao = new System.Windows.Forms.RadioButton();
+            this.rdbAdicao = new System.Windows.Forms.RadioButton();
             this.lblResultado = new System.Windows.Forms.Label();
             this.lblResposta = new System.Windows.Forms.Label();
             this.btnCalcular = new System.Windows.Forms.Button();
             this.btnLimpar = new System.Windows.Forms.Button();
             this.btnSair = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dtpCalendario = new System.Windows.Forms.DateTimePicker();
             this.gpbOperador.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -92,34 +94,23 @@ namespace Mobilitec_Services
             this.gpbOperador.Controls.Add(this.rdbsubtracao);
             this.gpbOperador.Controls.Add(this.rdbAdicao);
             this.gpbOperador.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gpbOperador.Location = new System.Drawing.Point(273, 235);
+            this.gpbOperador.Location = new System.Drawing.Point(320, 148);
             this.gpbOperador.Name = "gpbOperador";
             this.gpbOperador.Size = new System.Drawing.Size(221, 224);
             this.gpbOperador.TabIndex = 2;
             this.gpbOperador.TabStop = false;
             this.gpbOperador.Text = "Operador";
             // 
-            // rdbAdicao
+            // rdbDivisao
             // 
-            this.rdbAdicao.AutoSize = true;
-            this.rdbAdicao.Location = new System.Drawing.Point(47, 39);
-            this.rdbAdicao.Name = "rdbAdicao";
-            this.rdbAdicao.Size = new System.Drawing.Size(99, 24);
-            this.rdbAdicao.TabIndex = 3;
-            this.rdbAdicao.TabStop = true;
-            this.rdbAdicao.Text = "Adição (+)";
-            this.rdbAdicao.UseVisualStyleBackColor = true;
-            // 
-            // rdbsubtracao
-            // 
-            this.rdbsubtracao.AutoSize = true;
-            this.rdbsubtracao.Location = new System.Drawing.Point(47, 80);
-            this.rdbsubtracao.Name = "rdbsubtracao";
-            this.rdbsubtracao.Size = new System.Drawing.Size(120, 24);
-            this.rdbsubtracao.TabIndex = 4;
-            this.rdbsubtracao.TabStop = true;
-            this.rdbsubtracao.Text = "Subtração (-)";
-            this.rdbsubtracao.UseVisualStyleBackColor = true;
+            this.rdbDivisao.AutoSize = true;
+            this.rdbDivisao.Location = new System.Drawing.Point(47, 173);
+            this.rdbDivisao.Name = "rdbDivisao";
+            this.rdbDivisao.Size = new System.Drawing.Size(96, 24);
+            this.rdbDivisao.TabIndex = 6;
+            this.rdbDivisao.TabStop = true;
+            this.rdbDivisao.Text = "Divisão (/)";
+            this.rdbDivisao.UseVisualStyleBackColor = true;
             // 
             // rdbMultiplicacao
             // 
@@ -132,16 +123,27 @@ namespace Mobilitec_Services
             this.rdbMultiplicacao.Text = "Multipliação (*)";
             this.rdbMultiplicacao.UseVisualStyleBackColor = true;
             // 
-            // rdbDivisao
+            // rdbsubtracao
             // 
-            this.rdbDivisao.AutoSize = true;
-            this.rdbDivisao.Location = new System.Drawing.Point(47, 173);
-            this.rdbDivisao.Name = "rdbDivisao";
-            this.rdbDivisao.Size = new System.Drawing.Size(96, 24);
-            this.rdbDivisao.TabIndex = 6;
-            this.rdbDivisao.TabStop = true;
-            this.rdbDivisao.Text = "Divisão (/)";
-            this.rdbDivisao.UseVisualStyleBackColor = true;
+            this.rdbsubtracao.AutoSize = true;
+            this.rdbsubtracao.Location = new System.Drawing.Point(47, 80);
+            this.rdbsubtracao.Name = "rdbsubtracao";
+            this.rdbsubtracao.Size = new System.Drawing.Size(120, 24);
+            this.rdbsubtracao.TabIndex = 4;
+            this.rdbsubtracao.TabStop = true;
+            this.rdbsubtracao.Text = "Subtração (-)";
+            this.rdbsubtracao.UseVisualStyleBackColor = true;
+            // 
+            // rdbAdicao
+            // 
+            this.rdbAdicao.AutoSize = true;
+            this.rdbAdicao.Location = new System.Drawing.Point(47, 39);
+            this.rdbAdicao.Name = "rdbAdicao";
+            this.rdbAdicao.Size = new System.Drawing.Size(99, 24);
+            this.rdbAdicao.TabIndex = 3;
+            this.rdbAdicao.TabStop = true;
+            this.rdbAdicao.Text = "Adição (+)";
+            this.rdbAdicao.UseVisualStyleBackColor = true;
             // 
             // lblResultado
             // 
@@ -205,11 +207,35 @@ namespace Mobilitec_Services
             this.btnSair.UseVisualStyleBackColor = false;
             this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "",
+            "Clube A",
+            "Clube B",
+            "Clube C"});
+            this.comboBox1.Location = new System.Drawing.Point(336, 57);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(157, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // dtpCalendario
+            // 
+            this.dtpCalendario.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpCalendario.Location = new System.Drawing.Point(69, 231);
+            this.dtpCalendario.MinDate = new System.DateTime(2023, 3, 9, 0, 0, 0, 0);
+            this.dtpCalendario.Name = "dtpCalendario";
+            this.dtpCalendario.Size = new System.Drawing.Size(94, 20);
+            this.dtpCalendario.TabIndex = 11;
+            // 
             // frmCalculadoraSimples
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.dtpCalendario);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.btnSair);
             this.Controls.Add(this.btnLimpar);
             this.Controls.Add(this.btnCalcular);
@@ -249,5 +275,7 @@ namespace Mobilitec_Services
         private System.Windows.Forms.Button btnCalcular;
         private System.Windows.Forms.Button btnLimpar;
         private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dtpCalendario;
     }
 }
